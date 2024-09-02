7709f0440e93b90c1e054f2c39cb6c99ce4d85d0
aliajproject <ali.aj.project@gmail.com>
2024-09-02 18:36:40 +0400

    Add files via upload

---
+++ my_connect.php
@@ -0,0 +1,18 @@
+<?php
+
+// info users
+$my_localhost = 'localhost';
+$my_user = 'root';
+$my_password = 'root';
+$my_databases = 'categorys';
+$my_port = 3333;
+
+// $my_localhost, $my_user, $my_password, $my_databases, $my_port
+
+$conn = mysqli_connect($my_localhost, $my_user, $my_password, $my_databases, $my_port);
+
+if (!$conn) {
+    echo "XETA: MySQL-ə qoşulmaq alınmadı !" . mysqli_connect_error();
+}
+
+?>
